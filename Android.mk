90e3992e4aaef5f6ceb7fc9f454c6a358601e1aa
cmitatakis <cmitatakis@gmail.com>
2017-06-15 18:20:58 +0300

    android mk update

--- Android.mk
+++ Android.mk
@@ -26,6 +26,8 @@ LOCAL_AAPT_FLAGS += --extra-packages com.jaredrummler.android.device
 LOCAL_PACKAGE_NAME := KatsunaInfoServices
 #LOCAL_PROGUARD_FLAG_FILES := app/proguard-rules.pro
 
-LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_PROGUARD_FLAGS := -ignorewarnings -include build/core/proguard_basic_keeps.flags
+LOCAL_PROGUARD_ENABLED := nosystem
+LOCAL_PROGUARD_FLAG_FILES := app/proguard-rules.pro
 
 include $(BUILD_PACKAGE)
