32b5c5f17944a8d88cb3dc120af86e0c0a96d774
cmitatakis <1Ag2ptGTZt>
2017-06-29 17:04:53 +0300

    activity hide

--- app/src/main/java/com/katsuna/infoservices/MainActivity.java
+++ app/src/main/java/com/katsuna/infoservices/MainActivity.java
@@ -1,29 +1,18 @@
 package com.katsuna.infoservices;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 
 import com.katsuna.commons.utils.DeviceUtils;
-import com.katsuna.infoservices.Preferences.PreferencesProvider;
-import com.katsuna.infoservices.facade.RegisterFacade;
 
 public class MainActivity extends Activity {
 
     private boolean setupFinished = false;
-    private static final int PERMISSION_ALL = 1;
-    String[] PERMISSIONS = {Manifest.permission.READ_PHONE_STATE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        final RegisterFacade userFacade = PreferencesProvider.LoggedUserInfo();
 
         while (!setupFinished) {
 
@@ -35,14 +24,11 @@ public class MainActivity extends Activity {
                 }
                 super.onCreate(savedInstanceState);
 
-                if (!hasPermissions(this, PERMISSIONS) && (userFacade == null )  ) {
-                    ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
-                } else {
-                    Intent myIntent = new Intent(this, ReportingService.class);
-                    setupFinished = true;
-                    startService(myIntent);
-                    finish();
-                }
+                setupFinished = true;
+                Intent activityIntent = new Intent(getApplicationContext(),PermissionsActivity.class);
+                getApplicationContext().startActivity(activityIntent);
+                finish();
+
             } else {
                 try {
                     Thread.sleep(1000 * 60 * 1);
@@ -53,39 +39,5 @@ public class MainActivity extends Activity {
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_ALL: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    Intent myIntent = new Intent(this, ReportingService.class);
-                    startService(myIntent);
-                    finish();
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-        }
-    }
-
-
-    public static boolean hasPermissions(Context context, String... permissions) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
-            for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
 }
