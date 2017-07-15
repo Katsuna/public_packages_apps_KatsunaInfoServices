229f27433abce15694ede0eac0c5478416a01395
George G <kreach3r@users.noreply.github.com>
2017-07-15 19:25:42 +0200

    Disabling sending points of interest to reporting server 2/2
    This BuildConfig class should belong to the library, not the app, (it probably should be com.katsuna.infoservices.BuildConfig) but I'm removing it to keep the commits inline with KatsunaServices

--- app/src/main/java/com/katsuna/infoservices/Alarms/ReportingServerFunctions.java
+++ app/src/main/java/com/katsuna/infoservices/Alarms/ReportingServerFunctions.java
@@ -15,7 +15,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.jaredrummler.android.device.BuildConfig;
 import com.jaredrummler.android.device.DeviceName;
 import com.katsuna.infoservices.KatsunaInfoServicesApplication;
 import com.katsuna.infoservices.Preferences.PreferencesProvider;
