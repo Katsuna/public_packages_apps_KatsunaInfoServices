62ab83c17be6d99ba4f862e252fc76c9b82ca98d
George G <kreach3r@users.noreply.github.com>
2017-07-15 19:20:49 +0200

    AOSP: include KatsunaCommons into build

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+KATSUNA_COMMON_PATH := $(ANDROID_BUILD_TOP)/frameworks/KatsunaCommon
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
@@ -6,8 +7,15 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
 
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
+LOCAL_FULL_LIBS_MANIFEST_FILES := $(KATSUNA_COMMON_PATH)/commons/src/main/AndroidManifest.xml
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
 LOCAL_RESOURCE_DIR += frameworks/support/v7/appcompat/res
+
+# Include KatsunaCommon into this app
+LOCAL_REQUIRED_MODULES := KatsunaCommon
+LOCAL_STATIC_JAVA_LIBRARIES := KatsunaCommon
+# Include KatsunaCommon resources
+LOCAL_RESOURCE_DIR += frameworks/KatsunaCommon/commons/src/main/res
 
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
@@ -15,13 +23,15 @@ LOCAL_STATIC_JAVA_LIBRARIES += androidvolley
 LOCAL_STATIC_JAVA_LIBRARIES += googlegson
 
 LOCAL_STATIC_JAVA_AAR_LIBRARIES := androiddevicenames
+LOCAL_STATIC_JAVA_AAR_LIBRARIES += roundedimageview
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --generate-dependencies
+LOCAL_AAPT_FLAGS += --extra-packages com.katsuna.commons
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
 LOCAL_AAPT_FLAGS += --extra-packages com.jaredrummler.android.device
-
+LOCAL_AAPT_FLAGS += --extra-packages com.makeramen.roundedimageview
 
 LOCAL_PACKAGE_NAME := KatsunaInfoServices
 #LOCAL_PROGUARD_FLAG_FILES := app/proguard-rules.pro
