7f067218ef76100d8b138e4f4981ecd9b9a979a2
George G <kreach3r@gmail.com>
2018-09-10 14:12:27 +0300

    Android.mk: Use AAPT2

--- Android.mk
+++ Android.mk
@@ -1,43 +1,36 @@
 LOCAL_PATH := $(call my-dir)
-KATSUNA_COMMON_PATH := $(ANDROID_BUILD_TOP)/frameworks/KatsunaCommon
 include $(CLEAR_VARS)
 
+LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
 
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
-LOCAL_FULL_LIBS_MANIFEST_FILES := $(KATSUNA_COMMON_PATH)/commons/src/main/AndroidManifest.xml
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
-LOCAL_RESOURCE_DIR += frameworks/support/v7/appcompat/res
-LOCAL_RESOURCE_DIR += frameworks/support/v7/cardview/res
-LOCAL_RESOURCE_DIR += frameworks/support/design/res
-LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/app/src/main/res
 
 # Include KatsunaCommon into this app
 LOCAL_REQUIRED_MODULES := KatsunaCommon
-LOCAL_STATIC_JAVA_LIBRARIES := KatsunaCommon
-# Include KatsunaCommon resources
-LOCAL_RESOURCE_DIR += frameworks/KatsunaCommon/commons/src/main/res
 
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
-LOCAL_STATIC_JAVA_LIBRARIES += androidvolley
-LOCAL_STATIC_JAVA_LIBRARIES += googlegson
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-cardview
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    KatsunaCommon \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-cardview \
+    android-support-v7-recyclerview \
+    android-support-design
 
-LOCAL_STATIC_JAVA_AAR_LIBRARIES := androiddevicenames
-LOCAL_STATIC_JAVA_AAR_LIBRARIES += roundedimageview
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    androidvolley \
+    googlegson
+
+LOCAL_STATIC_JAVA_AAR_LIBRARIES := \
+    androiddevicenames \
+    roundedimageview
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --generate-dependencies
-LOCAL_AAPT_FLAGS += --extra-packages com.katsuna.commons
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
-LOCAL_AAPT_FLAGS += --extra-packages com.jaredrummler.android.device
-LOCAL_AAPT_FLAGS += --extra-packages com.makeramen.roundedimageview
 
 LOCAL_PACKAGE_NAME := KatsunaInfoServices
 #LOCAL_PROGUARD_FLAG_FILES := app/proguard-rules.pro
